31cbd31e2b5751d9641aa99ecb1d3305abad2ad1
zaki <omararturo.diazalarcon@upaep.edu.mx>
2023-03-31 09:55:26 -0600

    Main menu almost done Added rectangle for map aded top bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: MainPageComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
