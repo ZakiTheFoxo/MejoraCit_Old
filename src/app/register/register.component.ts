91013528310e89d675a70bfd38bf0a7868d94d7b
zaki <omararturo.diazalarcon@upaep.edu.mx>
2023-08-30 11:16:08 -0600

    register view

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  nombre: string = "";
   email: string = "";
+  cel: string = "";
   password: string = "";
+  password2: string = "";
+  check: boolean = false;
 
   constructor() { }
 
@@ -15,6 +19,6 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    
+
   }
 }
