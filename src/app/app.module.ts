b8d9de965ee0db074af338dba5468ba66052aa7a
zaki <omararturo.diazalarcon@upaep.edu.mx>
2023-09-08 11:17:13 -0600

    geolocation of user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,6 +25,7 @@ import { RegisterComponent } from './register/register.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: '', component: MainPageComponent },
       { path: 'reportes/:id', component: CaseDetailsComponent},
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { reportes } from '../reportes';
+// import { HttpClient  } from '@angular/common/http';
 
 @Component({
   selector: 'app-main-page',
@@ -13,6 +14,32 @@ export class MainPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+  }
+  // Esta funcion es para obtener la geolocalizacion del usuario
+  // api_key = '41b8410e4a384faf8f38f6a1bb29162b';
+  // url = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + this.api_key;
+
+  // ipAddress = "";
+
+  // city = "";
+
+  // region = "";
+
+  // country = "";
+
+  // constructor(private http:HttpClient) { }
+
+  // ngOnInit() {
+  //   this.getGeolocationData();
+  // }
+
+  // getGeolocationData() {
+  //   this.http.get(this.url).subscribe((res:any)=>{
+  //     this.ipAddress = res.ip_address;
+  //     this.city = res.city;
+  //     this.region = res.region;
+  //     this.country = res.country;
+  //   });
+  // }
 }
